875555508b1bbdcd57c24496e4c4e5f47755e88f
kajla2208 <142565353+kajla2208@users.noreply.github.com>
2024-04-08 09:14:34 +0530

    Add files via upload

---
+++ Day3/js/index.js
@@ -0,0 +1,134 @@
+// alert("hello");
+// function env(){
+// // console.log(message)
+
+// // var message = 'hello';
+
+// // console.log(message)
+
+// // var message='else';
+
+// // console.log(message)
+// // for(var i=0;i<5;i++){
+// //     console.log('--'+i);
+// // }
+// // console.log(i);
+// // }
+
+// // env();
+
+// //let
+// console.log(message);
+// let message="hello";
+// console.log(message);
+
+//CONST
+// const message;
+// message='else';
+// console.log(message);
+
+
+// message='else';
+// console.log(message);
+
+// let age='abcd';
+// age='bcde';
+// console.log(age);
+
+// let arr=['a','b','c','d'];
+
+// arr[0]='abcd';
+// console.log(arr);
+
+// let str='12';
+
+// let age=20;
+
+// let ans=age+str;
+// console.log(ans);
+
+// const str = 'khan';
+// const usrName = 'My name is: ';
+// const ans = `${usrName} ${str}`;
+// console.log(ans);
+
+// function print(str='nothing is here'){
+//     console.log(str);
+// }
+
+// const s='hello';
+// print();
+// function sum(a,b){
+//     if(a&&b){
+//         console.log(a+b);
+//     }
+//     else{
+//         console.log(a);
+//     }
+// }
+
+// sum(10);
+
+
+// let a='';
+// if(a){
+//     console.log('true');
+// }
+// else{
+//     console.log('false');
+// }
+// print();
+// // a();
+// // b();
+
+// //function declaretion
+// function print(){
+//     console.log('--');
+// }
+
+// // function print(){
+// //     console.log('++');
+// // }
+
+// //function assignment( named )
+// let a = function print(){
+//     console.log('--');
+// }
+
+// //function  assignment ( anonymous )
+// let b = function (){
+//     console.log('--');
+// }
+
+// //arrow function assignment
+// let c=  ()=> {console.log("--")};
+
+// print();
+// a();
+// b();
+
+// const obj = new Object();
+// obj[1]='one';
+// obj['2']="two";
+// obj["12"]="three";
+// console.log(obj);
+
+
+// const obj = {
+//     'firstname': 'Abk',
+//     'lastName': 'twelve',
+//     'age':20,
+// };
+// const fullName1 = obj.firstName + '' + obj.lastName;
+// const fullName2 = obj.firstName + '' + obj.lastName;
+// console.log(fullName1);
+
+const obj = {
+         'firstname' : 'Abk',
+         'lastName' : 'twelve',
+         'age':20,
+     };
+
+     const x = prompt('enter details');
+
+     console.log(obj[x]);
